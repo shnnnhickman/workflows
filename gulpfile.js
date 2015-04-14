7a6b1c7286211a6c7ad0673431ddd05ced4e6e23
Shannon Hickman <sh@sootsmacked.com>
2015-04-13 19:52:38 -0700

    concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 	
 var coffeeSrc = ['components/coffee/*.coffee'];
-	
+var jsSrc = [	'components/scripts/rclick.js',
+				'components/scripts/tagline.js'
+];
+
 gulp.task('coffee', function(){
-gulp.src(coffeeSrc)
+	gulp.src(coffeeSrc)
 	.pipe(coffee({ bare:true })
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSrc)
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
