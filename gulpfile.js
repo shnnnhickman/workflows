be78afe5b1eb757eb76d50a06740aea288c51db1
Shannon Hickman <sh@sootsmacked.com>
2015-05-09 18:23:10 -0700

    updated compass

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,40 @@ var gulp = require('gulp'),
 var coffeeSrc = 'components/coffee/*.coffee';
 var jsSrc = ['components/scripts/rclick.js',
     'components/scripts/template.js',
-    'components/scripts/tagline.js'];
+    'components/scripts/tagline.js'
+    ];
 /*quick test*/
 var sassSrc = ['components/sass/style.scss'];
 
-gulp.task('coffee', function() {   
-    gulp.src(coffeeSrc)
-                .pipe(coffee({  bare: true })
-                .on('error', gutil.log)) jslint,
+gulp.task('coffee', function() {
+            gulp.src(coffeeSrc)
+                .pipe(coffee({
+                        bare: true
+                    })
+                    .on('error', gutil.log))
                 .pipe(gulp.dest('components/scripts))
-                });
+});
 
-            gulp.task('js', function() {
+gulp.task('js', function() {
                 gulp.src(jsSrc)
-                .pipe(concat('scripts.js'))
+                    .pipe(concat('scripts.js'))
                     .pipe(browserify())
                     .pipe(gulp.dest('builds/development/js'))
-            });
+});
 
-            gulp.task('compass', function() {
+gulp.task('compass', function() {
                 gulp.src(sassSrc);
                 .pipe(compass({
                                 sass: 'components/sass',
                                 image: 'builds/development/images',
                                 style: 'expanded'
-                            }
-                            s)
+                            }))
                         .on('error', gutil.log))
-                    .pipe(gulp.dest('builds/development/css'));
-            });
+                    .pipe(gulp.dest('builds/development/css'))
+});
 
-            gulp.task('watch', function() {
+gulp.task('watch', function() {
                 gulp.watch(coffeeSrc, ['coffee'])
-            });
+});
 
             gulp.task('default', ['coffee', 'js', 'compass']);
